31e0f81a5681f9fb5a338c8afeab93d310893261
MichaelGhattas1 <89952560+MichaelGhattas1@users.noreply.github.com>
2022-01-02 17:15:35 +0200

    Update get-repo.service.ts

--- src/app/get-repo.service.ts
+++ src/app/get-repo.service.ts
@@ -8,8 +8,14 @@ import { Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class GetRepoService {
-
-  constructor( private _HttpClient:HttpClient ) { }
+  day:number = new Date().getUTCDate()
+  addZero:string=''
+  constructor( private _HttpClient:HttpClient ) { 
+   if(this.day<10)
+    {
+      this.addZero='0'
+    }
+  }
  getAllRep ( pageNum:number ,  year:number , month:number ,day:number  ) :Observable<any>
 {
   return this._HttpClient.get(`https://api.github.com/search/repositories?q=created:>${year}-${month}-${day}&sort=stars&order=desc&page=${pageNum}`)
